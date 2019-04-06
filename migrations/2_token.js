42c8e583c14c59d5a6f6af20a53facc84e40afd5
Fugue Foundation <fuguefoundation@gmail.com>
2019-04-06 11:10:06 -0500

    removed null contracts

--- migrations/2_token.js
+++ migrations/2_token.js
@@ -1,12 +1,11 @@
-// Contracts
-//const null1 = artifacts.require('./null1.sol');
-const TokenERC20 = artifacts.require('./TokenERC20.sol');
+//const TokenERC20 = artifacts.require('./TokenERC20.sol');
+const AdvancedToken = artifacts.require('./AdvancedToken.sol');
 
 module.exports = async (deployer, network) => {
 
     console.log(`${"-".repeat(30)}
-    DEPLOYING TokenERC20 Contract...\n
+    DEPLOYING AdvancedToken Contract...\n
     Using ` + network + ` network\n`);
 
-    deployer.deploy(TokenERC20, 100, "Fugue", "XFF");
+    deployer.deploy(AdvancedToken, 200, "DiploCoin", "DUN");
 };
