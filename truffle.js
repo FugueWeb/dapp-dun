f175b1095569438c3c1b523b48b161e0ad5d3014
Fugue Foundation <fuguefoundation@gmail.com>
2019-04-06 11:13:40 -0500

    removed rsk

--- truffle.js
+++ truffle.js
@@ -3,7 +3,6 @@ var HDWalletProvider = require('truffle-hdwallet-provider');
 
 var mnemonic = process.env.SEED;
 var infura = "https://ropsten.infura.io/" + process.env.INFURA_KEY;
-var publicNode = 'https://public-node.testnet.rsk.co:443';
 
 module.exports = {
   networks: {
@@ -16,21 +15,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
-    },       
-    rsk: {
-      provider: () =>
-        new HDWalletProvider(mnemonic, publicNode),
-      network_id: '*',
-      gas: 2500000,
-      gasPrice: 183000
     },
     ropsten: {
       provider: function() {
         return new HDWalletProvider(mnemonic, infura)
       },
       network_id: 3,
-      gas : 6700000,
-      gasPrice : 10000000000
+    //   gas : 6700000,
+    //   gasPrice : 10000000000
     }        
   }
 }
